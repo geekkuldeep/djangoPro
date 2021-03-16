99fa4c0e9e4f122db03abf349361f87460291738
geekkuldeep <kuldeep.pal@avantika.edu.in>
2021-03-16 16:11:38 +0530

    Commit
    Commit

---
+++ client_side/templates/temp_dir/navbar-4.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bgcolor-primary">
+          <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "stage1_ahhaa";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+
+?>
+   
+	
+</nav>
