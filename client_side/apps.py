0a6c329c997a935f14c9c09c26590d1c5cb56bc8
geekkuldeep <kuldeep.pal@avantika.edu.in>
2021-03-16 16:35:05 +0530

    NewComm
    NewComm

--- client_side/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class ClientSideConfig(AppConfig):
-    name = 'client_side'
-
--- client_side/templates/temp_dir/search-box.php
+++
@@ -1,7 +0,0 @@
-<form class="searchform" action="search-page.php" style="z-index:10;">
-  <input type="text" class="searchinput" placeholder="I am feeling....." name="search"  style="z-index:10;">
-  <!--<button type="submit" class="searchbutton"  style="z-index:10;"><i class="fa fa-search"></i></button>-->
-  <button type="submit" class="searchbutton"  style="z-index:10;"><img src='assets/images/white-bulb.png' class="bulb-search"/></button>
-
-</form>
-       
